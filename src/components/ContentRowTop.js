0fe857eb40baa686e81ec6e0ea9ff41be67586af
Juan Pablo Paillet <pailletjuanpablo@gmail.com>
2021-09-27 20:37:43 -0300

    Staging changes

---
+++ src/components/ContentRowTop.js
@@ -0,0 +1,74 @@
+import React, { useState, useEffect, useRef} from 'react';
+import ContentRowCenter from './ContentRowCenter';
+import ContentRowMovies from './ContentRowMovies';
+import Chart from './Chart';
+
+function ContentRowTop() {
+
+
+
+	// 1) Identificador del elemento en el estado
+	// 2) Método para actualizarlo
+	const [name, setName] = useState('Rodolfo');
+	const [count, setCount] = useState(0);
+
+	// useRef == querySelector()
+	// Seleccionar elementos del Virtual DOM
+
+	const divConDatos = useRef();
+
+	useEffect(
+		() => {
+			// Actualiza el título del documento usando la API del navegador
+			console.log(`You clicked ${count} times`);
+			// Lo que retorne == componentWillUnmount
+			return () => {
+				console.log('Componente destruido')
+			}
+		},
+		// Dependencias: Cambios en el valor de elementos del estado que van a disparar el useEffect
+		// Si dependencias == array vacío, es igual a componentDidMount()
+		[
+			
+		]
+	);
+
+
+
+	return (
+		<React.Fragment>
+			{/*<!-- Content Row Top -->*/}
+			<div className="container-fluid">
+
+				<div
+				
+				className="d-sm-flex aligns-items-center justify-content-between mb-4">
+					<h1 ref={divConDatos} className="h3 mb-0 text-gray-800">
+						
+						 {count} - {name} 
+						 
+						 </h1>
+
+					<button onClick={() => divConDatos.current.style.border = '1px solid red'}>
+						Click me
+     				</button>
+
+					<button ref={divConDatos} onClick={() => setName('Jorge')}>
+						Cambiar nombre
+     				</button>
+
+				</div>
+
+				{/*<!-- Content Row Movies-->*/}
+				<ContentRowMovies />
+				<ContentRowCenter />
+				<Chart />
+
+			</div>
+			{/*<!--End Content Row Top-->*/}
+
+		</React.Fragment>
+	)
+
+}
+export default ContentRowTop;
